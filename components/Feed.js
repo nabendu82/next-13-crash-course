6e1eaaae208585461f43940432b164d8cfc2904c
nabendu82 <nabendu.biswas@gmail.com>
2023-06-16 21:27:52 +0530

    Pre-final commit

--- components/Feed.js
+++ components/Feed.js
@@ -5,9 +5,23 @@ import PromptList from "./PromptList";
 
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
+  const [searchedResults, setSearchedResults] = useState([]);
   const [allPosts, setAllPosts] = useState([]);
-  const handleSearchChange = e => {}
-  const handleTagClick = tag => {}
+
+  const filterPrompts = text => allPosts.filter(item => item.prompt.includes(text) || item.tag.includes(text))
+
+  const handleSearchChange = e => {
+    setSearchText(e.target.value)
+    const searchResult = filterPrompts(e.target.value);
+    setSearchedResults(searchResult);
+  }
+
+  const handleTagClick = tag => {
+    setSearchText(tag);
+    const searchResult = filterPrompts(tag);
+    setSearchedResults(searchResult);
+  }
+
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await fetch("/api/prompt");
@@ -20,9 +34,9 @@ const Feed = () => {
   return (
     <section className='feed'>
       <form className='relative w-full flex-center'>
-        <input type='text' placeholder='Search for tag/username' value={searchText} onChange={handleSearchChange} required className='search_input peer' />
+        <input type='text' placeholder='Search for prompt/tag' value={searchText} onChange={handleSearchChange} required className='search_input peer' />
       </form>
-      <PromptList data={allPosts} handleTagClick={handleTagClick} />
+      {searchText ? <PromptList data={searchedResults} handleTagClick={handleTagClick} /> : <PromptList data={allPosts} handleTagClick={handleTagClick} />}
       </section>
   )
 }
